821e10c859779844db505ed9c6cab9497be4b73a
jatin0670 <jp0670786@gmail.com>
2025-04-17 09:43:23 +0530

    BubbleSort

--- practice.java
+++ practice.java
@@ -2,34 +2,22 @@ import java.util.*;
 
 class practice{
 
-    static int[] search(int[][]ar, int target){
-        int row = 0;
-        int col = ar.length-1;
-
-        while(row<ar.length && col>=0){
-              if(ar[row][col]==target){
-                return new int[] {row,col};
-              }
-              else if(ar[row][col]<target){
-                row++;
-              }
-              else{
-                col--;
-              }
-
+      static void BubS(int[] arr){
+        for(int i=0; i<arr.length; i++){
+          for(int j=1; j<=arr.length-i-1; j++){
+            if(arr[j]<arr[j-1]){
+              int temp = arr[j];
+              arr[j] = arr[j-1];
+              arr[j-1] = temp;
+            }
+          }
         }
-        return new int[] {-1,-1};
-    }
+      }
 
       public static void main(String args[]){
-      int[][] arr = {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-      };
-
-      int[] ans = search(arr, 9);
-      System.out.println(Arrays.toString(ans));
-      
+     
+        int[] a = {5,4,3,2,1};
+        BubS(a);
+        System.out.println(Arrays.toString(a));
     }
 }
